c08720bb1b8df9b107e275ce25b011881149d3d4
wanglong <1608550717@qq.com>
2024-08-18 17:56:55 +0800

    api 项目脚手架- first commit

---
+++ wl-api/src/main/java/com/wl/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.wl.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wl.project.model.entity.User;
+
+/**
+ * @Entity com.wl.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
